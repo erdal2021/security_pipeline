00ebf61d614eee39b22dd6e9adef0f8ecca20323
erdal2021 <erdal.erdogan@t-online.de>
2024-02-02 15:44:12 +0100

    Edit main.py line 12

--- main.py
+++ main.py
@@ -10,22 +10,22 @@ app = Flask(__name__)
 def exec_command():
     # Direkte Ausführung von Benutzereingaben ohne Validierung
     command = request.args.get('ls')
-    subprocess.call(command, shell=True)
+    result: subprocess.run(command, check=True)
     return "Kommando ausgeführt\n"
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    # Unsichere Deserialisierung von Benutzereingaben
-    file = request.files['file'].read()
-    data = pickle.loads(file)
-    return "Datei hochgeladen\n"
+# @app.route('/upload', methods=['POST'])
+# def upload_file():
+#     # Unsichere Deserialisierung von Benutzereingaben
+#     file = request.files['file'].read()
+#     data = pickle.loads(file)
+#     return "Datei hochgeladen\n"
 
-@app.route('/run', methods=['POST'])
-def run_command():
-    command = request.form['command']
-    # Unsichere Verwendung von os.system für Benutzereingaben
-    os.system(command)
-    return "Kommando ausgeführt\n"
+# @app.route('/run', methods=['POST'])
+# def run_command():
+#     command = request.form['command']
+#     # Unsichere Verwendung von os.system für Benutzereingaben
+#     os.system(command)
+#     return "Kommando ausgeführt\n"
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5000)
